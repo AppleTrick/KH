525a6747a5662557cdd6e32566996bbe6b4d889c
Changhee Park <arrans9591@gmail.com>
2021-03-07 23:27:51 +0900

    client server connect request

--- Jsp10_Ajax01/src/com/ajax/controller/ScoreController.java
+++ Jsp10_Ajax01/src/com/ajax/controller/ScoreController.java
@@ -1,11 +1,15 @@
 package com.ajax.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
 
 
 @WebServlet("/ScoreController")
@@ -20,6 +24,36 @@ public class ScoreController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
+		
+		String name = request.getParameter("name");
+		int kor = Integer.parseInt(request.getParameter("kor"));
+		int eng = Integer.parseInt(request.getParameter("eng"));
+		int math = Integer.parseInt(request.getParameter("math"));
+		
+		int sum = kor + eng + math;
+		double avg = (double)sum /3;
+		
+		
+		
+		// json - simple - 1.1.1 jar 파일에 존재
+		// json object를 생성
+		
+		JSONObject obj = new JSONObject();
+		obj.put("name",name);
+		obj.put("sum",sum);
+		obj.put("avg",String.format("%.2f", avg));
+		
+		//{"name" : name , "sum" : sum , "math" : math } 형태로 존재하게 된다.
+		
+		System.out.println("server에서 보내는 데이터 : " + obj.toJSONString());
+		//toJSONString() : json 형식의 문자열로 만듬  - toJSONString 을 안할 경우 주소 값이 보내지게된다.
+		
+		PrintWriter out = response.getWriter();
+		out.println(obj.toJSONString());
+		// msg에 {"avg":"100.00","name":"박창희","sum":300} json 객체로 넘어간다 이는 dataType 의 형태로 지정된다.
+		
+		
+		
 	}
 
 }
--- Jsp10_Ajax02/WebContent/resources/js/score.js
+++ Jsp10_Ajax02/WebContent/resources/js/score.js
@@ -10,10 +10,10 @@ function getParameterValues() {
 function load(){
     var url = "score.do"+getParameterValues();
 
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", url, true);
-    httpRequest.onreadystatechange = callback;
-    httpRequest.send();
+    httpRequest = new XMLHttpRequest();			// server와 통신하는 것을 도와주는 객체 , 보통 XHR 이라고 칭함 -> 
+	httpRequest.onreadystatechange = callback;	// 처리할 함수 onredaystatechange 이벤트가 발생할때마다. callback 함수 호출
+    httpRequest.open("GET", url, true);			// true : 비동기 / false 동기
+    httpRequest.send();							// get : send() / post : send("data")
 }
 
 function callback(){
@@ -21,6 +21,8 @@ function callback(){
     if(httpRequest.readyState == 4 ){
         alert("status : " +httpRequest.status);
         if (httpRequest.status == 200) {
+		
+			// responseText: 요청 후 응답받은 문자열
             var obj = JSON.parse(httpRequest.responseText);
             document.getElementById("result").innerHTML = decodeURIComponent(obj.name) + "의 총점 : " + obj.sum + "\n평균 : " + obj.avg;
         } else {
@@ -28,3 +30,35 @@ function callback(){
         }
     }
 }
+
+/*
+	XMTHttpRequest : javascript Object .htpp를 통한 데이터 송수신 지원
+	
+	
+	<onreadystatechange>
+	- readystate
+	0 : unintialized - 실행되지 않음
+	1 : loading - 로드중 
+	2 : loaded - 로드 됨
+	3 : interactive - 동신됨
+	4 : complete - 통신 완료
+	
+	
+	- status
+	200 : 성공
+	400 : badrequest
+	401 : unauthorized
+	403 : forbidden
+	404 : not found
+	415 : unsupported media type
+	500 : internal server error
+
+
+	encodeURIComponent : 모든 문자를 인코딩 (UTF-8)
+	decodeURIComponent : UTF-8 에서 다시 원래 글자로
+	encodeURI : queryString (주소줄 ) 에서 사용되는 특수문자는 제외하고 인코딩
+	
+	JSON.parse : JSON 형태의 문자열을 json 객체로 변환 string -> jsonObject 로 변환
+	JSON.stringify : javascript 객체 (json 형태로 변환 할 수 있는 ) 를 json 형태의 문자열로 변환 object -> json String
+	
+*/
